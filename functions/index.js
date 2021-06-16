a8964aa1c529c8983cea1c092e8b31b1f129c6b6
fkd <fukuda.developer@gmail.com>
2021-06-16 22:50:36 +0900

    正常にレスポンスを返却するように修正。

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,4 @@
 const functions = require("firebase-functions");
-const googleAPI = require("googleapis");
 const cors = require('cors')({origin: true});
 const { GoogleSpreadsheet } = require('google-spreadsheet');
 
@@ -23,11 +22,17 @@ exports.app = functions.region('asia-northeast2').https.onRequest((req, res) => 
             	return res.status(400).send('Bad requested, rates of parameter was empty');
             }
 			
-			loadRateInfo(currency, rates);
+			console.log('start');
+			loadRateInfo(currency, rates).then(json => {
+				res.set("Access-Control-Allow-Origin", "*");
+				console.log(json);
+				res.json(json);
+				console.log('end');
+			});
         })
 })
 
-async function loadRateInfo(currency, rateTargetList) {
+async function loadRateInfo(currency, rates) {
 	
 	let doc;
 	switch (currency) {
@@ -48,8 +53,30 @@ async function loadRateInfo(currency, rateTargetList) {
 	});
 	
 	await doc.loadInfo();
-	console.log(doc.title);
-	await doc.updateProperties({ title: 'renamed doc' });
+// 	console.log(doc.title);
+// 	await doc.updateProperties({ title: 'renamed doc' });
+
+	// シート読み込み＆対象通貨読み込み
+	const sheet = doc.sheetsByTitle['RATE'];
+	await sheet.loadCells(['F2:F144', 'C2:C144']);
+	
+	let result = {};
+	//	XXXAAA形式の2カ国通貨コードのセルを一つづつ読み込み、リクエストされた取得対象のレートであれば連想配列に追加する
+	for (let i = 2; i <= 144; i++) {
+		let key = 'F' + i;
+		const target = sheet.getCellByA1(key).value;
+		
+		console.log("key:" + key + "target:" + target);
+		//	要求された通貨であるか調べて、結果を取得
+		if (rates.includes(target)) {
+			const rate = sheet.getCellByA1('C' + i).value;
+			result[target] = rate;
+			console.log("currency:" + target + ", rate:" + rate);
+		}
+	}
+	
+	console.log(result);
+	return result;
 }
 
 
