29e0318fa84e42061ab68a982e0ab15fbbff6174
fkd <fukuda.developer@gmail.com>
2021-08-08 21:43:06 +0900

    いらん関数を削除。

--- functions/index.js
+++ functions/index.js
@@ -5,9 +5,6 @@ if (!process.env.FUNCTION_NAME || process.env.FUNCTION_NAME === 'rates') {
 if (!process.env.FUNCTION_NAME || process.env.FUNCTION_NAME === 'test') {
     exports.test		= require('./test');
 }
-if (!process.env.FUNCTION_NAME || process.env.FUNCTION_NAME === 'test2') {
-    exports.test2	= require('./test2');
-}
 if (!process.env.FUNCTION_NAME || process.env.FUNCTION_NAME === 'all_rates') {
     exports.all_rates	= require('./all_rates');
 }
